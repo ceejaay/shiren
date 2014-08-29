f2a73e0d20cae7439ead155abf76137b8d15c868
Chad Jemmett <chad.jemmett@gmail.com>
2014-08-29 14:43:36 -0600

    see individual scores. updated DB schema. adjusted forms to match DB

--- shiren.rb
+++ shiren.rb
@@ -7,10 +7,13 @@ DataMapper::setup(:default, "postgres://localhost/shiren")
 class DungeonRun
   include DataMapper::Resource
   property :id, Serial
+  property :created_at, DateTime
+  property :user, Text 
   property :dungeon, Text, :required => true
   property :score, Integer, :required => true
+  property :killed_by, Text 
   property :complete, Boolean, :required => true, :default => false
-  property :level, String, :required => true
+  property :level, Text, :required => true
   property :experience, Text, :required => true
   property :hitpoints, Text, :required => true
   property :strength, Text, :required => true
@@ -18,6 +21,7 @@ class DungeonRun
   property :sword, Text, :required => true
   property :shield, Text, :required => true
   property :armband, Text, :required => true
+  property :approved, Boolean, :default => false
   
 DataMapper.finalize.auto_upgrade!
 
