e3810f1f581f091a8d0c7c5e688030b1eae140d1
Chad Jemmett <chad.jemmett@gmail.com>
2015-04-21 13:53:47 -0600

    made many changes. Not sure what

--- shiren.rb
+++ shiren.rb
@@ -1,31 +1,13 @@
 require 'sinatra'
-require 'data_mapper'
-
-DataMapper::setup(:default, "postgres://localhost/shiren")
+require './shiren_model'
 
 
-class DungeonRun
-  include DataMapper::Resource
-  property :id, Serial
-  property :created_at, DateTime
-  property :user, Text 
-  property :dungeon, Text, :required => true
-  property :score, Integer, :required => true
-  property :killed_by, Text 
-  property :complete, Boolean, :required => true, :default => false
-  property :level, Text, :required => true
-  property :experience, Text, :required => true
-  property :hitpoints, Text, :required => true
-  property :strength, Text, :required => true
-  property :gitans, Text, :required => true
-  property :sword, Text, :required => true
-  property :shield, Text, :required => true
-  property :armband, Text, :required => true
-  property :approved, Boolean, :default => false
-
-
+get "/dungeons"  do
+  data = params[:title]
+  @run = DungeonRun.all(:dungeon => data)
+  @data = params
+  erb :by_dungeon
 end
-DataMapper.finalize.auto_upgrade!
 
 get "/" do
   @run = DungeonRun.all :order => :score.desc
@@ -37,8 +19,10 @@ post "/" do
   redirect "/"
 end
 
+
 get "/:id" do 
   @run = DungeonRun.get params[:id]
+  @data = params
   erb :show
 end
 
@@ -49,6 +33,7 @@ end
 post "/user/new" do
   
 end
+
 
 
 
